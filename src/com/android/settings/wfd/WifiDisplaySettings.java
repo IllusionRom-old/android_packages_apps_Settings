9c75e82bea737770db61d0574ff44d3add1ac444
aznrice <aznrice2k4@gmail.com>
2013-12-07 01:54:17 -0600

    Revert "Add help url for remote display."
    This reverts commit 2408d65c085aafdf0903d9ae6a92a9b3da5212c8.

--- src/com/android/settings/wfd/WifiDisplaySettings.java
+++ src/com/android/settings/wfd/WifiDisplaySettings.java
@@ -137,11 +137,6 @@ public final class WifiDisplaySettings extends SettingsPreferenceFragment {
     }
 
     @Override
-    protected int getHelpResource() {
-        return R.string.help_url_remote_display;
-    }
-
-    @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
